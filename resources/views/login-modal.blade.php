023131a1f6e4e36ca450d6eeaba7507efb717822
David Huerta <dhuerta23@gmail.com>
2016-05-14 21:54:26 +0000

    header & login modal

---
+++ resources/views/login-modal.blade.php
@@ -0,0 +1,38 @@
+<div id="loginModal" class="modal fade" role="dialog">
+    <div class="modal-dialog" role="document">
+      <!-- Modal Content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Login </h4>
+        </div>
+        <div class="modal-body">
+          <form class="form-inline"  >
+
+            <div class="form-group">
+              <label for="email" class="sr-only  control-label">Last Name</label>
+              <div>
+                <input type="text" class="form-control input-sm" id="email" name="email" placeholder="Email">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="password" class="sr-only control-label">password</label>
+              <div>
+                <input type="password" class="form-control input-sm" id="password" name="password" placeholder="Password">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="checkbox">
+                <label class="checkbox-inline" >
+                  <input type="checkbox" name="remember" value="">
+                  <strong> Remember me </strong>
+                </label>
+              </div>
+            </div>
+            <button type="button" class="btn btn-info btn-sm">Sign in</button>
+            <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Sing up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+</div>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
-<html ng-app="myApp">
+<html ng-app="myApp" xmlns:javascript="http://www.w3.org/1999/xhtml">
 <head>
-  <meta charset="UTF-8">
-  <title>RAUMBILD WYSIWYG</title>
-  <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-  <script>var baseUrl = "{{ url('/') }}/";</script>
+    <meta charset="UTF-8">
+    <title>RAUMBILD WYSIWYG</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+    <script>var baseUrl = "{{ url('/') }}/";</script>
 </head>
 <body>
-<nav>nav</nav>
-<div class="main-container">
-  <div id="nav-container">nav-container</div>
-  <div id="editor-container">editor-container</div>
-</div>
-<footer></footer>
-<script type="text/javascript" src="{{ asset('bower_components/angular/angular.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('bower_components/angular-route/angular-route.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    @include('header')
+    @include('login-modal')
+
+    <div class="main-container">
+        <div id="nav-container">nav-container</div>
+        <div id="editor-container">editor-container</div>
+    </div>
+    @include('footer')
+    <script type="text/javascript" src="{{ asset('bower_components/angular/angular.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/angular-route/angular-route.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 </body>
 </html>
