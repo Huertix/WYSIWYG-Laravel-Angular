6e389983c028c728ce791f632eb822aadfe25081
David Huerta <dhuerta23@gmail.com>
2016-05-16 16:56:12 +0000

    bootstrap responsive

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -15,7 +15,7 @@
     <div ng-include src="'views/login-modal.html'"></div>
     <div ng-include src="'views/save-modal.html'"></div>
 
-    <div class="container main-container " ng-view></div>
+    <div class="container main-container" ng-view></div>
 
     <div ng-include src="'views/footer.html'"></div>
 
