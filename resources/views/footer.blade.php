dbd4d7b7284ed040d1d852da8a058f02ec16c88b
David Huerta <dhuerta23@gmail.com>
2016-05-17 08:58:23 +0000

    cleaning

--- resources/views/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="row-footer">
-    <div class="container">
-        <div class="col-xs-12">
-            <p style="padding:10px;"></p>
-            <p align=center>by David Huerta</p>
-        </div>
-    </div>
-</footer>
